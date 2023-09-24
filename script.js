b0fd617980d4dd5bbd186a2a8d114642ef89a487
shubham-bhutada <137187942+shubham-bhutada@users.noreply.github.com>
2023-09-24 14:02:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+const btn = document.getElementById("submit");
+btn.addEventListener("click", volume_sphere);
 function volume_sphere() {
-    //Write your code here
-  
-} 
+  //Write your code here
+  const rad = document.getElementById("radius");
+  const vol = document.getElementById("volume");
+  let r = Number(rad.value);
+  console.log(r);
+  let totalVol = (4 / 3) * 3.14 * r * r * r;
+  console.log(totalVol);
+  vol.value = totalVol;
+  console.log(vol.value);
+//   vol.innerText = vol.value;
+  // alert(vol.innerText)
+}
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
