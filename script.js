79c85b82f38923669009447d0b50a6e623083b5f
shubham-bhutada <137187942+shubham-bhutada@users.noreply.github.com>
2023-10-06 13:36:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,10 @@ const btn = document.getElementById("submit");
 btn.addEventListener("click", volume_sphere);
 function volume_sphere() {
   //Write your code here
-  const rad = document.getElementById("radius");
-  const vol = document.getElementById("volume");
-  let r = Number(rad.value);
-  console.log(r);
-  let totalVol = (4 / 3) * 3.14 * r * r * r;
-  console.log(totalVol);
-  vol.value = totalVol;
-  console.log(vol.value);
-//   vol.innerText = vol.value;
-  // alert(vol.innerText)
-}
+	const r = document.getElementById("radius");
+	const v = document.getElementById("volume");
+	let radius = parseInt(r.value);
+	let result = (4/3)*3.14*Math.pow(radius,3);
+	v.value = result;
+  }
 
